5957b43f2d885b88f8c3114cf3783a5f1a91bc9d
nathanrubeda <ubedanathan@gmail.com>
2024-11-13 13:59:26 -0500

    Initial Commit

---
+++ Pokedex/PokedexApp.swift
@@ -0,0 +1,17 @@
+//
+//  PokedexApp.swift
+//  Pokedex
+//
+//  Created by Nathan Ubeda on 11/13/24.
+//
+
+import SwiftUI
+
+@main
+struct PokedexApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
